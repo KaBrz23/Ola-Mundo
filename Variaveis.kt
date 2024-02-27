c059f365ec2e994a89eff05ce1a2c48ad680d4ae
KaBrz23 <kauefernandesbraz@gmail.com>
2024-02-26 21:46:48 -0300

    variaveis

---
+++ Variaveis.kt
@@ -0,0 +1,27 @@
+// Exemplo de variável em Kotlin
+
+// fun main() {
+//     val nome = "Kauê" // Variavel tipo String // pode ser alterada
+//     var idade = 18 // Variavel do tipo inteira // pode ser alterada
+
+//     println("Nome: $nome, Idade: $idade") // imprimindo o conteúdo da variável no console
+// }
+
+//Declaração e Inicialização de Variáveis
+//Int, Double, String, Boolean - Mais utilizadas
+
+fun main () {
+    val numeroInteiro: Int = 10
+    var numeroDecimal: Double = 3.14
+    val texto: String = "Olá Mundo!"
+    var status: Boolean = true //se true o status é on-line se falso status é off-line (0 ou 1)
+
+    println("NúmeroInteiro: $numeroInteiro")
+    println("NúmeroDecimal: $numeroDecimal")
+    println("Texto: $texto")
+    println("Status: $status")
+
+
+    
+
+}
